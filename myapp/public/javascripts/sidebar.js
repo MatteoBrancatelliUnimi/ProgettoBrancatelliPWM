d91a08c3ab6af5485cf3c3237856b17d6a0875a5
Matteo Brancatelli <matteo.brancatelli@studenti.unimi.it>
2022-04-27 15:44:07 +0200

    Sidebar now is invisible on <=790px

--- myapp/public/javascripts/sidebar.js
+++ myapp/public/javascripts/sidebar.js
@@ -3,15 +3,22 @@ sidebar = body.querySelector('nav'),
 toggle = body.querySelector(".toggle"),
 searchBtn = body.querySelector(".search-box"),
 modeSwitch = body.querySelector(".toggle-switch"),
-modeText = body.querySelector(".mode-text");
+modeText = body.querySelector(".mode-text"),
+openSidebar = body.querySelector('.sidebarOpen');
 
 
 toggle.addEventListener("click" , () =>{
 sidebar.classList.toggle("close");
+sidebar.classList.toggle("visible");
 })
 
 searchBtn.addEventListener("click" , () =>{
 sidebar.classList.remove("close");
+})
+
+openSidebar.addEventListener("click" , () =>{
+  sidebar.classList.remove("close");
+  sidebar.classList.add("visible");
 })
 
 modeSwitch.addEventListener("click" , () =>{
